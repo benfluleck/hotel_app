e6e7864c2a138aee75ca9d9b420ca44fd0e16683
benfluleck <benfluleck@gmail.com>
2025-03-12 17:10:45 +0100

    feat(hotel): Connection module - Create Hotel entity

--- src/hotel/entities/hotel.entity.ts
+++ src/hotel/entities/hotel.entity.ts
@@ -1,1 +1,34 @@
-export class Hotel {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'hotels' })
+export class Hotel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text', unique: true })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  street_address: string;
+
+  @Column({ type: 'text', nullable: true })
+  state: string;
+
+  @Column({ type: 'text', nullable: true })
+  email: string;
+
+  @Column({ type: 'text', nullable: true })
+  phone: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
